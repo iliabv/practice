f47e55e6bb1088621caaa22862f01c349b7796be
Ilia Babanov <ilya.babanov@gmail.com>
2026-03-10 21:20:54 +0100

    Tune srs

--- js/spaced-repetition.js
+++ js/spaced-repetition.js
@@ -6,8 +6,8 @@ export function updateSR(word, correct) {
   let { easeFactor = 2.5, interval = 1 } = word;
 
   if (correct) {
-    interval = interval <= 1 ? 6 : Math.round(interval * easeFactor);
-    easeFactor = Math.max(1.3, easeFactor + 0.1);
+    interval = interval <= 1 ? 1 : Math.round(interval * easeFactor);
+    easeFactor = Math.min(2.5, Math.max(1.3, easeFactor + 0.05));
   } else {
     interval = 0;
     easeFactor = Math.max(1.3, easeFactor - 0.3);
